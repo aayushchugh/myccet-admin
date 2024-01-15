b00d137696bcf351bf0b3a7f1846f909bc1e81ea
Ayush Chugh <ayushchugh2006@gmail.com>
2024-01-15 10:50:07 +0530

    header ui

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Layout as AntLayout } from "antd";
+import { Layout as AntLayout, Flex, Typography } from "antd";
 import SideBar from "./components/sideBar/SideBar";
 import { Outlet } from "react-router-dom";
 
@@ -8,6 +8,14 @@ const Layout: FC = () => {
 		<AntLayout rootClassName="min-h-screen" hasSider>
 			<SideBar />
 			<AntLayout>
+				<AntLayout.Header className="bg-white flex items-center justify-end">
+					<Flex justify="end" align="center">
+						{/* TODO: get name from API */}
+						<Typography.Title level={3} className="m-0 !mb-0">
+							Hey Ayush!
+						</Typography.Title>
+					</Flex>
+				</AntLayout.Header>
 				<AntLayout.Content style={{ margin: "0 16px" }}>
 					<Outlet />
 				</AntLayout.Content>
